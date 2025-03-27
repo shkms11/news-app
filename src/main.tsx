94fbe6f06811652005801f36e55dcae364e6c407
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-03-27 19:48:26 +0600

    wrap the app with provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
@@ -9,6 +11,8 @@ const root = createRoot(rootElement);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
