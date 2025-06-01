85d62b56eb660d3387850ce4d86683561ce26a0f
Shamsul <48136141+shkms11@users.noreply.github.com>
2025-06-02 00:52:55 +0600

    update config files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     environment: 'jsdom',
     globals: true,
